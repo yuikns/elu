08938654320aaf2708d65f38c4bb9fd3ffd3f1c6
Yu Jing <yujing5b5d@gmail.com>
2018-04-23 15:01:18 -0400

    upgrade deps

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,25 +16,36 @@ const cards = [
         "link": "http://blog.argcv.com/",
         "title": "The Blog",
         "content": "Something Interested by Yu",
-        "thumb": "https://cdn.worldvectorlogo.com/logos/wordpress-icon.svg"
+        "thumb": "https://cdn.worldvectorlogo.com/logos/wordpress-icon.svg",
+    },
+    {
+        "link": "http://dev.argcv.com/",
+        "title": "The Repository",
+        "content": "The Repository Hosted by ArgCV",
+        "thumb": "https://cdn.worldvectorlogo.com/logos/gitlab.svg",
     },
     {
         "link": "http://github.com/yuikns",
         "title": "The Github Page",
         "content": "Yu's Public Repositories",
-        "thumb": "https://cdn.worldvectorlogo.com/logos/github-icon.svg"
+        "thumb": "https://cdn.worldvectorlogo.com/logos/github-icon.svg",
     },
     {
-        "link": "https://gitter.im/argcv/argcv",
+        "link": "http://gitter.im/argcv/argcv",
         "title": "The Gitter Room",
-        "content": "Gitter is an open source instant messaging and chat room system for developers and users of GitHub repositories.",
-        "thumb": "https://cdn.worldvectorlogo.com/logos/gitter.svg"
+        "content": "Gitter is an open source instant messaging and chat room system for developers and users of GitHub repositories",
+        "thumb": "https://cdn.worldvectorlogo.com/logos/gitter.svg",
     },
     {
-        "link": "https://blog.argcv.com/guestbook",
+        "link": "http://blog.argcv.com/gallery",
+        "title": "The Gallery",
+        "content": "A Few Valuable Links Listed by Yu",
+    },
+    {
+        "link": "http://blog.argcv.com/guestbook",
         "title": "The Guest Book",
-        "content": "Anything wish to talk with Me? Please Click here!"
-    }
+        "content": "Anything wish to talk with Me? Please Click here!",
+    },
 ]
 
 export default ({ match }) => {
